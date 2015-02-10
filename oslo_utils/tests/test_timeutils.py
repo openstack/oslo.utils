548b64020816af6400b51d6f0da0685b36213295
Joshua Harlow <harlowja@yahoo-inc.com>
2015-02-09 16:50:53 -0800

    Add a stopwatch + split for duration(s)
    The existing timeutils provides a way to measure durations
    that is also used to get actual times and dates; for most
    cases this dual ability is not needed and the user only cares
    about the duration between two time points. In cases where
    ntpd may affect the machine clock this usage is also incorrect
    and may go backwards in time...
    
    To support this kind of specific and frequently occurring usage
    a stopwatch class can be used that can enable more accurate
    gathering of *just* durations (and using monotonic time when
    it's available in python 3.3+ or optionally when the library
    at http://pypi.python.org/pypi/monotonic is installed).
    
    This code comes from taskflow, but oslo.messaging has similar
    code that they could replace with this (and I imagine tooz
    could also use this); among others...
    
    Change-Id: I950e26c1eb8283a134fb4f16544f87bc5d29fd82

--- oslo_utils/tests/test_timeutils.py
+++ oslo_utils/tests/test_timeutils.py
@@ -25,6 +25,12 @@ from testtools import matchers
 from oslo_utils import timeutils
 
 
+def monotonic_iter(start=0, incr=0.05):
+    while True:
+        yield start
+        start += incr
+
+
 class TimeUtilsTest(test_base.BaseTestCase):
 
     def setUp(self):
@@ -344,3 +350,159 @@ class TestIso8601Time(test_base.BaseTestCase):
         dtn = datetime.datetime(2011, 2, 14, 19, 53, 7)
         naive = timeutils.normalize_time(dtn)
         self.assertTrue(naive < dt)
+
+
+class StopWatchTest(test_base.BaseTestCase):
+    def test_leftover_no_duration(self):
+        watch = timeutils.StopWatch()
+        watch.start()
+        self.assertRaises(RuntimeError, watch.leftover)
+        self.assertRaises(RuntimeError, watch.leftover, return_none=False)
+        self.assertIsNone(watch.leftover(return_none=True))
+
+    def test_no_states(self):
+        watch = timeutils.StopWatch()
+        self.assertRaises(RuntimeError, watch.stop)
+        self.assertRaises(RuntimeError, watch.resume)
+
+    def test_bad_expiry(self):
+        self.assertRaises(ValueError, timeutils.StopWatch, -1)
+
+    @mock.patch('oslo_utils.timeutils.now')
+    def test_backwards(self, mock_now):
+        mock_now.side_effect = [0, 0.5, -1.0, -1.0]
+        watch = timeutils.StopWatch(0.1)
+        watch.start()
+        self.assertTrue(watch.expired())
+        self.assertFalse(watch.expired())
+        self.assertEqual(0.0, watch.elapsed())
+
+    @mock.patch('oslo_utils.timeutils.now')
+    def test_expiry(self, mock_now):
+        mock_now.side_effect = monotonic_iter(incr=0.2)
+        watch = timeutils.StopWatch(0.1)
+        watch.start()
+        self.assertTrue(watch.expired())
+
+    @mock.patch('oslo_utils.timeutils.now')
+    def test_not_expired(self, mock_now):
+        mock_now.side_effect = monotonic_iter()
+        watch = timeutils.StopWatch(0.1)
+        watch.start()
+        self.assertFalse(watch.expired())
+
+    def test_has_started_stopped(self):
+        watch = timeutils.StopWatch()
+        self.assertFalse(watch.has_started())
+        self.assertFalse(watch.has_stopped())
+        watch.start()
+
+        self.assertTrue(watch.has_started())
+        self.assertFalse(watch.has_stopped())
+
+        watch.stop()
+        self.assertTrue(watch.has_stopped())
+        self.assertFalse(watch.has_started())
+
+    def test_no_expiry(self):
+        watch = timeutils.StopWatch(0.1)
+        self.assertRaises(RuntimeError, watch.expired)
+
+    @mock.patch('oslo_utils.timeutils.now')
+    def test_elapsed(self, mock_now):
+        mock_now.side_effect = monotonic_iter(incr=0.2)
+        watch = timeutils.StopWatch()
+        watch.start()
+        matcher = matchers.GreaterThan(0.19)
+        self.assertThat(watch.elapsed(), matcher)
+
+    def test_no_elapsed(self):
+        watch = timeutils.StopWatch()
+        self.assertRaises(RuntimeError, watch.elapsed)
+
+    def test_no_leftover(self):
+        watch = timeutils.StopWatch()
+        self.assertRaises(RuntimeError, watch.leftover)
+        watch = timeutils.StopWatch(1)
+        self.assertRaises(RuntimeError, watch.leftover)
+
+    @mock.patch('oslo_utils.timeutils.now')
+    def test_pause_resume(self, mock_now):
+        mock_now.side_effect = monotonic_iter()
+        watch = timeutils.StopWatch()
+        watch.start()
+        watch.stop()
+        elapsed = watch.elapsed()
+        self.assertAlmostEqual(elapsed, watch.elapsed())
+        watch.resume()
+        self.assertNotEqual(elapsed, watch.elapsed())
+
+    @mock.patch('oslo_utils.timeutils.now')
+    def test_context_manager(self, mock_now):
+        mock_now.side_effect = monotonic_iter()
+        with timeutils.StopWatch() as watch:
+            pass
+        matcher = matchers.GreaterThan(0.04)
+        self.assertThat(watch.elapsed(), matcher)
+
+    @mock.patch('oslo_utils.timeutils.now')
+    def test_context_manager_splits(self, mock_now):
+        mock_now.side_effect = monotonic_iter()
+        with timeutils.StopWatch() as watch:
+            time.sleep(0.01)
+            watch.split()
+        self.assertRaises(RuntimeError, watch.split)
+        self.assertEqual(1, len(watch.splits))
+
+    def test_splits_stopped(self):
+        watch = timeutils.StopWatch()
+        watch.start()
+        watch.split()
+        watch.stop()
+        self.assertRaises(RuntimeError, watch.split)
+
+    def test_splits_never_started(self):
+        watch = timeutils.StopWatch()
+        self.assertRaises(RuntimeError, watch.split)
+
+    @mock.patch('oslo_utils.timeutils.now')
+    def test_splits(self, mock_now):
+        mock_now.side_effect = monotonic_iter()
+
+        watch = timeutils.StopWatch()
+        watch.start()
+        self.assertEqual(0, len(watch.splits))
+
+        watch.split()
+        self.assertEqual(1, len(watch.splits))
+        self.assertEqual(watch.splits[0].elapsed,
+                         watch.splits[0].length)
+
+        watch.split()
+        splits = watch.splits
+        self.assertEqual(2, len(splits))
+        self.assertNotEqual(splits[0].elapsed, splits[1].elapsed)
+        self.assertEqual(splits[1].length,
+                         splits[1].elapsed - splits[0].elapsed)
+
+        watch.stop()
+        self.assertEqual(2, len(watch.splits))
+
+        watch.start()
+        self.assertEqual(0, len(watch.splits))
+
+    @mock.patch('oslo_utils.timeutils.now')
+    def test_elapsed_maximum(self, mock_now):
+        mock_now.side_effect = [0, 1] + ([11] * 4)
+
+        watch = timeutils.StopWatch()
+        watch.start()
+        self.assertEqual(1, watch.elapsed())
+
+        self.assertEqual(11, watch.elapsed())
+        self.assertEqual(1, watch.elapsed(maximum=1))
+
+        watch.stop()
+        self.assertEqual(11, watch.elapsed())
+        self.assertEqual(11, watch.elapsed())
+        self.assertEqual(0, watch.elapsed(maximum=-1))
