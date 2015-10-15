e5a8182a52eb47662ac8c61acabb1e9ee0fc1712
Victor Stinner <vstinner@redhat.com>
2015-10-15 16:14:14 +0200

    Write document for each unit of oslo_utils.utils
    Change-Id: I2b062cb18cb324dead60d45de98a48ad2ed421e4

--- oslo_utils/units.py
+++ oslo_utils/units.py
@@ -19,20 +19,36 @@ Unit constants
 
 # Binary unit constants.
 Ki = 1024
+"Binary kilo unit"
 Mi = 1024 ** 2
+"Binary mega unit"
 Gi = 1024 ** 3
+"Binary giga unit"
 Ti = 1024 ** 4
+"Binary tera unit"
 Pi = 1024 ** 5
+"Binary peta unit"
 Ei = 1024 ** 6
+"Binary exa unit"
 Zi = 1024 ** 7
+"Binary zetta unit"
 Yi = 1024 ** 8
+"Binary yotta unit"
 
 # Decimal unit constants.
 k = 1000
+"Decimal kilo unit"
 M = 1000 ** 2
+"Decimal mega unit"
 G = 1000 ** 3
+"Decimal giga unit"
 T = 1000 ** 4
+"Decimal tera unit"
 P = 1000 ** 5
+"Decimal peta unit"
 E = 1000 ** 6
+"Decimal exa unit"
 Z = 1000 ** 7
+"Decimal zetta unit"
 Y = 1000 ** 8
+"Decimal yotta unit"
