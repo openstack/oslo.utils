b9938230f992935e8332b6e288937be890724cd2
Vladyslav Drok <vdrok@mirantis.com>
2020-06-08 18:39:36 +0200

    Release greenthread when computing checksum
    Having time.sleep(0) after reading each block of data for computing
    checksum will allow other greenthreads to run.
    
    Closes-Bug: 1882569
    Change-Id: I6e547d206de9e3e333e29ccad52bf6b519a86ff9

--- oslo_utils/tests/test_fileutils.py
+++ oslo_utils/tests/test_fileutils.py
@@ -19,6 +19,8 @@ import os
 import shutil
 import stat
 import tempfile
+import time
+from unittest import mock
 import uuid
 
 from oslotest import base as test_base
@@ -213,6 +215,23 @@ class TestComputeFileChecksum(test_base.BaseTestCase):
 
         actual_checksum = fileutils.compute_file_checksum(path)
 
+        self.assertEqual(expected_checksum.hexdigest(), actual_checksum)
+
+    def test_compute_checksum_sleep_0_called(self):
+        path = fileutils.write_to_tempfile(self.content)
+        self.assertTrue(os.path.exists(path))
+        self.check_file_content(self.content, path)
+
+        expected_checksum = hashlib.sha256()
+        expected_checksum.update(self.content)
+
+        with mock.patch.object(time, "sleep") as sleep_mock:
+            actual_checksum = fileutils.compute_file_checksum(
+                path, read_chunksize=4)
+
+        sleep_mock.assert_has_calls([mock.call(0)] * 3)
+        # Just to make sure that there were exactly 3 calls
+        self.assertEqual(3, sleep_mock.call_count)
         self.assertEqual(expected_checksum.hexdigest(), actual_checksum)
 
     def test_compute_checksum_named_algorithm(self):
