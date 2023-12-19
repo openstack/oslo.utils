85e998e766df5b04145b87487c3bd158003fc6b9
Takashi Kajinami <kajinamit@oss.nttdata.com>
2023-12-19 13:34:18 +0900

    is_valid_ipv4: Enable strict check by default
    The default behavior which disables the strict check was deprecated in
    4.8.0[1], which was released during wallaby cycle.
    
    [1] 3288539a0b3be6b69efd2630b79133b918bb079b
    
    Related-Bug: #1914386
    Change-Id: Ia04b8774c85a519d9695aebe8b956920f9915495

--- oslo_utils/tests/test_netutils.py
+++ oslo_utils/tests/test_netutils.py
@@ -156,27 +156,19 @@ class NetworkUtilsTest(test_base.BaseTestCase):
 
     @mock.patch.object(netutils, 'LOG', autospec=True)
     def test_is_valid_ipv4(self, mock_log):
-        expected_log = 'Converting in non strict mode is deprecated. ' \
-            'You should pass strict=False if you want to preserve ' \
-            'legacy behavior'
         self.assertTrue(netutils.is_valid_ipv4('42.42.42.42'))
 
         self.assertFalse(netutils.is_valid_ipv4('-1.11.11.11'))
 
         self.assertFalse(netutils.is_valid_ipv4(''))
 
-        self.assertTrue(netutils.is_valid_ipv4('10'))
-        mock_log.warning.assert_called_with(expected_log)
-        mock_log.reset_mock()
-        self.assertTrue(netutils.is_valid_ipv4('10.10'))
-        mock_log.warning.assert_called_with(expected_log)
-        mock_log.reset_mock()
-        self.assertTrue(netutils.is_valid_ipv4('10.10.10'))
-        mock_log.warning.assert_called_with(expected_log)
-        mock_log.reset_mock()
+        self.assertFalse(netutils.is_valid_ipv4('10'))
+        self.assertFalse(netutils.is_valid_ipv4('10.10'))
+        self.assertFalse(netutils.is_valid_ipv4('10.10.10'))
         self.assertTrue(netutils.is_valid_ipv4('10.10.10.10'))
         mock_log.warning.assert_not_called()
         mock_log.reset_mock()
+
         self.assertFalse(
             netutils.is_valid_ipv4('10', strict=True)
         )
